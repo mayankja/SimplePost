8369d2d7dfb5a7e4e2e7736bbf704b456ca07257
Mayank <maynkkummar675@gmail.com>
2022-06-29 17:43:33 +0530

    Fix Spec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: "User"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'with valid attributes' do
-    subject(:user) { described_clas.new(first_name: 'John', job_title: 'Software Engineer') }
+    subject(:user) { described_class.new(first_name: 'John', email: 'john@test.com') }
 
     it 'is valid' do
       expect(user).to be_valid
@@ -15,11 +15,8 @@ RSpec.describe User, type: :model do
     subject(:user) { described_class.new }
 
     it 'requires email' do
+      expect(user.valid?).to eq(false)
       expect(user.errors[:email]).to include("can't be blank")
-    end
-
-    it 'requires job_title' do
-      expect(user.errors[:job_title]).to include("can't be blank")
     end
   end
 end
